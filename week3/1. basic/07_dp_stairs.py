57e34ace10a8e076cf35e469625650c9f9710f54
SongRaeHyeok <amorslh02130@gmail.com>
2026-09-07 15:17:50 +0900

    Solve Week3 basic 07 dp - 계단 오르기

--- week3/1. basic/07_dp_stairs.py
+++ week3/1. basic/07_dp_stairs.py
@@ -46,14 +46,16 @@ def climb_stairs(n):
         n번째 계단까지 오르는 방법의 수
     """
     # TODO: 특별한 경우 처리
-    pass
     
+    if n <= 1 :
+        return 1
     
     # TODO: dp 배열 생성 및 초기화
-    pass
+    dp = [1, 1]
+    for i in range(2, n + 1):
+            dp.append(dp[i - 1] + dp[i - 2])
     
     # TODO: 작은 문제부터 차례로 계산
-    pass
     
     return dp[n]
 
